798435fc6898835af63f2cbd9eb07f65af5c18af
Karuna1630 <karunagiri481@gmail.com>
2025-06-25 13:23:35 +0545

    update contactus page in which message print in console

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -12,10 +12,7 @@ import JobRecommendation from './Pages/JobRecommendation'
 import CompanyR1 from './Pages/CompanyR1'
 import MessagesInbox from './Pages/MessagesInbox'
 import CompanyR2 from './Pages/CompanyR2'
-
 import CompanyR3 from './Pages/CompanyR3'
-import LoginComponent from './Components/LoginComponent.jsx'
-
 import ContactUs from './Pages/ContactUs'
 
 
@@ -42,10 +39,7 @@ function App() {
           <Route path='/companyR1' element={<CompanyR1/>}/>
           <Route path='/messagesinbox' element={<MessagesInbox/>}/>
           <Route path='/companyR2' element={<CompanyR2/>}/>
-
           <Route path='/companyR3' element={<CompanyR3/>}/>
-          <Route path='/logincomp' element={<LoginComponent/>}/>
-
           <Route path='/contactus' element={<ContactUs/>}/>
 
 
